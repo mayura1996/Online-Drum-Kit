f6ab0cf263fd3d1568bbf69c2d9c18233a6d8175
mayura1996 <mmayurapavan@gmail.com>
2020-12-18 16:56:28 +0530

    done

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
     var buttonInnerHTML = this.innerHTML;
-  makeSound(buttonInnerHTML)
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
-document.addEventListener("keydown",function(event){
-  makeSound(event.key)
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 
 
-function makeSound(key){
+function makeSound(key) {
 
   switch (key) {
     case "w":
@@ -46,8 +48,18 @@ function makeSound(key){
       kick.play();
       break;
 
-    default: console.log(buttonInnerHTML)
+    default:
+      console.log(buttonInnerHTML);
   }
+}
 
 
+function buttonAnimation (currentKey) {
+var activeButton = document.querySelector("."+currentKey);
+activeButton.classList.add("pressed");
+
+
+setTimeout(function(){
+  activeButton.classList.remove("pressed");
+}, 100);
 }
